07a8e3ca4493a230494cb8b665ec36f1c72799fa
Mphela Neo Mengwai <mnmengwai@gmail.com>
2022-06-19 16:41:06 +0200

    inittial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+
+// IP Address Tracker
+
+//   Search for any IP address or domain
+
+//   IP Address
+//   Location
+//   Timezone
+//     UTC <!-- add offset value dynamically using the API -->
+//   ISP
